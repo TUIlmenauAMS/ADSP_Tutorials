e42d7860b813710f855843f8d1ab229017341dd5
rprofeta <30694071+rprofeta@users.noreply.github.com>
2022-10-07 20:38:58 +0200

    Update nbgrader_config.py

--- seminars/nbgrader_config.py
+++ seminars/nbgrader_config.py
@@ -1,6 +1,8 @@
 #According to: https://nbgrader.readthedocs.io/en/stable/user_guide/managing_assignment_files.html#setting-up-the-exchange
+# Template for the nbgrader configuration file
+# You must change the data to fit your system
 c = get_config()
-c.CourseDirectory.course_id = "adsp_ws2022"
-c.Exchange.root = "/home/fera/WS22_Seminars/instructor/nextcloud"
-c.CourseDirectory.student_id = "jimihendrix"
+c.CourseDirectory.course_id = "adsp_ws2022" # Don't change this!!!
+c.Exchange.root = "/home/username/WS22_Seminars/instructor/nextcloud" # Change this to point to your local folder (nextcloud).
+c.CourseDirectory.student_id = "6666927" # Change this to your matrikel number.
 
