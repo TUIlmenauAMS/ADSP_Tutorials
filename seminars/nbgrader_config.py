b547f936eac7a8a8e5e92598572dbe29e7f5cf3f
rprofeta <30694071+rprofeta@users.noreply.github.com>
2022-10-07 20:36:12 +0200

    Add files via upload

---
+++ seminars/nbgrader_config.py
@@ -0,0 +1,6 @@
+#According to: https://nbgrader.readthedocs.io/en/stable/user_guide/managing_assignment_files.html#setting-up-the-exchange
+c = get_config()
+c.CourseDirectory.course_id = "adsp_ws2022"
+c.Exchange.root = "/home/fera/WS22_Seminars/instructor/nextcloud"
+c.CourseDirectory.student_id = "jimihendrix"
+
